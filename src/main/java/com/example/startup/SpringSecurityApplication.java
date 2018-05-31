3a8aac94933405cad5674b536300f1de5e759dd6
Subrata1920 <>
2018-05-31 14:56:13 +0530

    v2

--- src/main/java/com/example/startup/SpringSecurityApplication.java
+++ src/main/java/com/example/startup/SpringSecurityApplication.java
@@ -15,6 +15,8 @@ public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
+		
+		System.out.println("test");
 	}
 }
 
